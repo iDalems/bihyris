e6d409f664604887867a055469e9ed3e56a1d118
Dalems <104506943+iDalems@users.noreply.github.com>
2026-04-26 02:23:36 +0300

    Delete backend directory

--- backend/src/auth/dto/magic-link.dto.ts
+++
@@ -1,8 +0,0 @@
-import { ApiProperty } from '@nestjs/swagger';
-import { IsEmail, IsString } from 'class-validator';
-
-export class MagicLinkDto {
-  @ApiProperty({ description: 'Email address for magic link' })
-  @IsEmail()
-  email: string;
-}
--- backend/src/guilds/dto/update-guild.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateGuildDto } from './create-guild.dto';
-
-export class UpdateGuildDto extends PartialType(CreateGuildDto) {}
